c22f791565a346be5c1cefc36c947c074d651428
Iara Miranda <iaramiranda@live.com>
2012-06-07 01:37:48 -0300

    declare var, in MY_Controller's construct

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,8 +2,13 @@
 
 class MY_Controller extends CI_Controller {
 
+    public $controller;
+    
     function __construct($config = array()) {
         parent::__construct();
+        
+        $this->controller = get_class($this);
+        $this->data['controller'] = $this->controller;
         
         if(isset($config['model']) && !empty($config['model'])){
             $this->load->model($config['model'], 'default_model');
@@ -12,6 +17,7 @@ class MY_Controller extends CI_Controller {
 
 
     public function add() {
+        //pre($_POST,1);
         $_inserted = $this->default_model->insert($_POST, TRUE);
         if(isAjax()){
             echo $_inserted ? 'sucess' : 'error';
